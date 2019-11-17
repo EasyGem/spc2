a6baae45eecfa82c92cb3ef13845b2a3e8b9b11d
Alexander Gusev <san4es021@gmail.com>
2019-11-17 16:25:21 +0300

    ok

--- static/mail.php
+++ static/mail.php
@@ -2,7 +2,7 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$pohne = $_POST['phone'];
+$phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent="Сообщение с сайта SPCon\n\nИмя: $name \n\nПочта: $email \n\nТелефон: $phone \nСообщение: $message";
 $recipient = "san4es-ag@yandex.ru";
